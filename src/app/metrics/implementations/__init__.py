a50a742ac44c16610771099f5c42912808ff87a8
Agentredx1 <Simon.gx01@gmail.com>
2025-09-13 13:23:00 -0400

    Metrics Scaffolding
    Scaffolding for the required metrics, currently just empty shells with stable_01 in base_metric.py creating dummy output

---
+++ src/app/metrics/implementations/__init__.py
@@ -0,0 +1,8 @@
+# Import all metric implementations to trigger registration
+from . import license
+from . import documentation  
+from . import maintainer
+from . import performance
+from . import size
+from . import dataset
+from . import code_quality
