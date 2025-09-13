207082ca8a8ab68b7419870d5bc05fd9b0ee9238
Luke Miller <88352100+LA-Miller@users.noreply.github.com>
2025-09-12 23:22:47 -0400

    Delete src/tests directory

--- src/tests/test_compile_all.py
+++
@@ -1,9 +0,0 @@
-import compileall
-from pathlib import Path
-
-# Checks that all files in src compile
-def test_all_python_files_compile():
-    src = Path("src")
-    assert src.exists(), "Expected a src/ folder"
-    ok = compileall.compile_dir(str(src), quiet=1)
-    assert ok, "Some files failed to compile."
