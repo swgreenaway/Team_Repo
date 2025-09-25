4822c78ad6360dccf3837d6a3ddd582d5f0726b6
Luke Miller <88352100+LA-Miller@users.noreply.github.com>
2025-09-25 19:11:55 -0400

    Is this the one

--- Logger.py
+++ Logger.py
@@ -43,8 +43,11 @@ def validate_log_file_or_die() -> None:
 log_file = os.getenv("LOG_FILE")  # no default; don't silently create a file
 log_level_env = os.getenv("LOG_LEVEL", "0")
 
+_level_map = {"0": logging.CRITICAL + 1, "1": logging.INFO, "2": logging.DEBUG}
+log_level = _level_map.get(log_level_env, logging.CRITICAL + 1)
+
 # FAIL FAST if LOG_FILE is set but invalid (must exist and be writable)
-validate_log_file_or_die()
+# validate_log_file_or_die()
 
 kwargs = dict(
     level=log_level,
@@ -53,7 +56,7 @@ kwargs = dict(
 
 # only attach a file handler if LOG_FILE was provided
 if log_file:
-    kwargs.update(filename=log_file, filemode="r+")  # don't create new files
+    kwargs.update(filename=log_file, filemode="a")   # don't create new files
 
 logging.basicConfig(**kwargs)
 
